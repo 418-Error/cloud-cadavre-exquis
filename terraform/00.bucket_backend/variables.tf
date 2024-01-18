a9de744e9c1dd5c3157349e1d800b781165c104d
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-01-18 11:44:30 +0100

    feat: s3 bucket

---
+++ terraform/00.bucket_backend/variables.tf
@@ -0,0 +1,21 @@
+variable ovh_conf {
+  type = map(object({
+    application_key = string
+    application_secret = string
+    consumer_key = string
+    project_id = string
+    }))
+  description = "OVH credentials"
+}
+
+variable ovh_endpoint {
+  type = string
+  description = "OVH endpoint"
+  default = "ovh-eu"
+}
+
+variable ovh_region {
+  type = string
+  description = "OVH region"
+  default = "gra"
+}
