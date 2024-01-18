9d65c1fddf8dc55eee5bd1a194a1bfeb1a44097f
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-01-18 20:17:08 +0100

    WIP: flux

---
+++ terraform/modules/flux_cd/variables.tf
@@ -0,0 +1,24 @@
+variable git_url {
+    type = string
+    description = "Git URL"
+}
+
+variable cluster_host {
+    type = string
+    description = "Cluster host"
+}
+
+variable cluster_ca_certificate {
+    type = string
+    description = "Cluster CA certificate"
+}
+
+variable cluster_client_certificate {
+    type = string
+    description = "Cluster client certificate"
+}
+
+variable cluster_client_key {
+    type = string
+    description = "Cluster client key"
+}
