c94f4f179b6229ea2cb0a17f61fcc06181f4e771
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-03-13 18:38:01 +0100

    feat: added vpc

---
+++ terraform/01.vpc/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "tfstate-bucket-prod"
+    key    = "terraform.tfstate"
+    region = "fr-par"
+    endpoints = {
+     s3: "https://s3.fr-par.scw.cloud"
+    }
+    skip_credentials_validation = true
+    skip_requesting_account_id = true
+    skip_region_validation      = true
+    skip_metadata_api_check = true
+    skip_s3_checksum = true
+  }
+}
