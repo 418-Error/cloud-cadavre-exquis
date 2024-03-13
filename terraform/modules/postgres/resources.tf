d43fe70046c784795f3c836f5c1fe660d66a45b9
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-03-13 18:39:38 +0100

    postgres module

---
+++ terraform/modules/postgres/resources.tf
@@ -0,0 +1,33 @@
+resource "scaleway_rdb_database" "database" {
+  instance_id    = scaleway_rdb_instance.instance.id
+  name           = "database"
+  region         = var.region
+}
+
+resource "random_password" "db_password" {
+  length  = 16
+  special = true
+}
+
+resource "scaleway_secret" "db_password" {
+  name        = "db-password"
+  description = "database password"
+  tags        = ["env=${terraform.workspace}"]
+}
+
+resource "scaleway_secret_version" "db_password_version1" {
+  description = "secret version 1"
+  secret_id   = scaleway_secret.db_password.id
+  data        = random_password.db_password.result
+}
+
+resource "scaleway_rdb_instance" "instance" {
+  name           = "instance-database"
+  node_type      = var.node_type
+  engine         = var.engine
+  is_ha_cluster  = false
+  disable_backup = false
+  user_name      = var.user_name
+  password       = random_password.db_password.result
+  volume_type    = "lssd"
+}
---
+++ terraform/modules/postgres/variables.tf
@@ -0,0 +1,23 @@
+variable "engine" {
+  description = "The database engine to use"
+  type        = string
+  default     = "PostgreSQL-15"
+}
+
+variable "user_name" {
+  description = "The username to use for the database"
+  type        = string
+}
+
+variable "node_type" {
+  description = "The type of node to use for the database"
+  type        = string
+  default     = "db-dev-s"
+}
+
+variable "region" {
+  description = "The region to use for the database"
+  type        = string
+  default     = "fr-par"
+}
+
